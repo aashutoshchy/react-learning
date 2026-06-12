3a3e042955b1cd79f912e259a87c8a099c69d272
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-06-12 23:04:18 +0545

    Initializing folder for Password Generator

--- 05ToDoListApp/src/App.jsx
+++ 05ToDoListApp/src/App.jsx
@@ -1,54 +1,56 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
 
 function App() {
 
-  const [todo, setTodo] = useState("")
-  const [todos, setTodos] = useState([])
+  const [tasks, setTasks] = useState(["Eat", "Drink"]);
+  const [newTask, setNewTask] = useState("");
 
-
-  const handleEdit = () => {
-
+  function handleInputChange(e) {
+    setNewTask(e.target.value);
   }
 
-  const handleDelete = () => {
-
+  function addTask() {
+    if (newTask.trim() !== "") setTasks(t => [...t, newTask])
+    setNewTask("")
   }
 
-  const handleAdd = () => {
-    setTodos(...todos, { todo, isCompleted: false })
-    setTodo("")
-    console.log(todos)
+  function deleteTask(index) {
+    const updatedTask = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTask);
   }
 
-  const handleChange = () => {
-    setTodo(e.target.value)
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTask = [...tasks];
+      [[index], [index - 1]]
+    }
+  }
+
+  function moveTaskDown(index) {
+
   }
 
   return (
     <>
-      <Navbar />
-      <div className="container bg-violet-50 mx-auto my-5 rounded-xl py-4 px-3 min-h-[80vh]">
-        <h2 className='text-lg font-bold'>Add a Todo</h2>
-        <div className="addTodo my-5">
-          <input onChange={handleChange} className='bg-white outline-none px-5 py-2 rounded-l-md w-2/4' type="text" />
-          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 px-5 py-2 text-white rounded-r-md'>Add</button>
+      <div>
+        <h1>To Do List</h1>
+        <div>
+          <input type="text" placeholder='Enter a Task' value={newTask} onChange={handleInputChange} />
+          <button onClick={() => addTask()}>Add</button>
         </div>
-        <div className=''>
-          <h2 className='text-lg font-bold'>{todo}</h2>
-          <div className="todos">
-            {todos.map((item) => {
-              <div className="todo flex" >
-                <div className="text">{item.todo}</div>
-                <div className="buttons">
-                  <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 px-3 py-1 text-white rounded-md mx-2'>Edit</button>
-                  <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 px-2 py-1 text-white rounded-md mx-2'>Delete</button>
-                </div>
-              </div>
-            })}
-          </div>
-        </div>
-      </div >
+        <ol>
+          {
+            tasks.map((task, index) => (
+              <li key={index}>
+                {task}
+                <button onClick={() => deleteTask(index)}>Delete</button>
+                <button onClick={() => moveTaskUp(index)}>⬆️</button>
+                <button onClick={() => moveTaskDown(index)}>⬇️</button>
+              </li>
+            ))
+          }
+        </ol>
+      </div>
     </>
   )
 }
